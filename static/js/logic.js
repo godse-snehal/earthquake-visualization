432d404901ad676cf865b0e937693201826fbe37
Snehal Godse <sgodse3@gmail.com>
2019-10-02 23:23:23 -0700

    modified size and color of the circle markers

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,24 +1,39 @@
+function getColor(d) {
+  // Numbers must be in descending order
+  return d >= 5 ? "#B22222" : 
+    d >= 4 ? "#DC143C" :
+    d >= 3 ? "#FF4500" :
+    d >= 2 ? "#FFA500" :
+    d >= 1 ? "#FFFF00" :
+    d >= 0 ? "#ADFF2F" :
+    'grey';
+}
+
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(url, function(response) {
 
   dataArray = response['features'];
-
+  console.log(dataArray);
   var locationMarkers = [];
 
   for (var i = 0; i < dataArray.length; i++) {
 
     coordinates = dataArray[i].geometry.coordinates;
     latlng = coordinates.slice(0, 2);
+    latlng = [latlng[1],latlng[0]];
+
+    magnitude = dataArray[i].properties.mag;
     
     locationMarkers.push(
       L.circle(latlng, {
-        stroke: false,
+        stroke: true,
         fillOpacity: 0.75,
-        color: "yellow",
-        fillColor: "yellow",
-        radius: dataArray[i].properties.mag
-      })
+        weight: 0.2,
+        color: 'black',
+        fillColor: getColor(magnitude),
+        radius: magnitude * 10000
+      }).bindPopup(dataArray[i].properties.title)
     );
   
   }
@@ -60,7 +75,7 @@ function createMap(earthquakes) {
   // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
-      37.09, -95.71
+      39.742043, -104.991531
     ],
     zoom: 5,
     layers: [streetmap, earthquakes]
@@ -70,6 +85,31 @@ function createMap(earthquakes) {
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps).addTo(myMap);
-}
 
+  // Create a legend to display information about our map
+  var Legend = L.Control.extend({  
+    options: {
+      position: 'bottomright'
+    },
+    onAdd: function (map) {
+      // Insert a div with the class of "legend"
+      var legend = L.DomUtil.create('div', 'info legend', L.DomUtil.get('map'));
+  
+      // Add legend elements
+      labels = ["0", "1", "2", "3", "4", "5"];
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < labels.length; i++) {
+        legend.innerHTML += '<i style="background:' + getColor(labels[i]) + '"></i> ' +
+                            labels[i] + (labels[i + 1] ? '&ndash;' + labels[i + 1] + '<br>' : '+');
+        }
+  
+      return legend;
+    }
+  });
+  
+  
+  myMap.addControl(new Legend());
+
+}
 
